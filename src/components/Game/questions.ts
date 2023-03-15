af59612ef5e86b7d70945a0d37174cd5eaf0089d
Nora Liu <noraliu0830@gmail.com>
2023-03-15 18:03:58 +0800

    feat: extra question

---
+++ src/components/Game/questions.ts
@@ -0,0 +1,177 @@
+export const questions = [
+  {
+    id: 1,
+    text: "你收到了一封來自不明電子郵件的貨幣推薦，並且該推薦聲稱可以讓你賺取更多的錢。你會怎麼做？",
+    options: [
+      {
+        id: 1,
+        text: "點擊電子郵件中的連結以了解更多信息。",
+        isCorrect: false,
+        moneyLost: 200000,
+      },
+      { id: 2, text: "刪除電子郵件。", isCorrect: true, moneyLost: 0 },
+    ],
+  },
+  {
+    id: 2,
+    text: "你想要買入一個名為XYZ的新加密貨幣。當你搜索並找到相關的網站時，你發現它需要你提供你的私鑰。你會怎麼做？",
+    options: [
+      {
+        id: 3,
+        text: "提供私鑰，以便購買新貨幣。",
+        isCorrect: false,
+        moneyLost: 500000,
+      },
+      {
+        id: 4,
+        text: "不提供私鑰，尋找其他購買方式。",
+        isCorrect: true,
+        moneyLost: 0,
+      },
+    ],
+  },
+  {
+    id: 3,
+    text: "你想在加密貨幣交易所買進或賣出一種貨幣，但你不知道最佳的買入或賣出價格。你會怎麼做？",
+    options: [
+      {
+        id: 5,
+        text: "以當前市場價格下單。",
+        isCorrect: false,
+        moneyLost: 300000,
+      },
+      {
+        id: 6,
+        text: "進行研究並查找適當的買入或賣出價格。",
+        isCorrect: true,
+        moneyLost: 0,
+      },
+    ],
+  },
+  {
+    id: 4,
+    text: "你收到了一個看起來像是加密貨幣錢包的軟體，它聲稱可以幫助你存儲和管理加密貨幣。你會怎麼做？",
+    options: [
+      {
+        id: 7,
+        text: "下載並使用該軟體。",
+        isCorrect: false,
+        moneyLost: 100000,
+      },
+      {
+        id: 8,
+        text: "進行研究並確認軟體的可信度和安全性。",
+        isCorrect: true,
+        moneyLost: 0,
+      },
+    ],
+  },
+  {
+    id: 5,
+    text: "你在社交媒體上看到一個聲稱可以幫忙你翻倍加密貨幣的廣告。你會怎麼做？",
+    options: [
+      {
+        id: 9,
+        text: "點擊廣告並遵從其指示。",
+        isCorrect: false,
+        moneyLost: 250000,
+      },
+      {
+        id: 10,
+        text: "忽略廣告，進行研究並確認該方案的可信度。",
+        isCorrect: true,
+        moneyLost: 0,
+      },
+    ],
+  },
+  {
+    id: 6,
+    text: "你收到了一個聲稱可以幫助你更快地挖礦加密貨幣的軟體。你會怎麼做？",
+    options: [
+      {
+        id: 11,
+        text: "下載並使用該軟體。",
+        isCorrect: false,
+        moneyLost: 150000,
+      },
+      {
+        id: 12,
+        text: "進行研究並確認該軟體的可信度和安全性。",
+        isCorrect: true,
+        moneyLost: 0,
+      },
+    ],
+  },
+  {
+    id: 7,
+    text: "你收到了一個看起來像是加密貨幣交易所的電子郵件，該電子郵件聲稱你的帳戶存在安全問題，需要進行驗證。你會怎麼做？",
+    options: [
+      {
+        id: 13,
+        text: "點擊電子郵件中的連結並進行驗證。",
+        isCorrect: false,
+        moneyLost: 200000,
+      },
+      {
+        id: 14,
+        text: "忽略該郵件，並登入交易所進行帳戶驗證。",
+        isCorrect: true,
+        moneyLost: 0,
+      },
+    ],
+  },
+  {
+    id: 8,
+    text: "你想要購買一個新的加密貨幣，但你不知道該如何選擇一個可信的交易所。你會怎麼做？",
+    options: [
+      {
+        id: 15,
+        text: "隨便選擇一個交易所。",
+        isCorrect: false,
+        moneyLost: 300000,
+      },
+      {
+        id: 16,
+        text: "進行研究並查找適當的交易所。",
+        isCorrect: true,
+        moneyLost: 0,
+      },
+    ],
+  },
+  {
+    id: 9,
+    text: "你有一個錢包，其中存儲了一些加密貨幣。你忘記了你的密碼。你會怎麼做？",
+    options: [
+      {
+        id: 17,
+        text: "嘗試多次輸入密碼。",
+        isCorrect: false,
+        moneyLost: 400000,
+      },
+      {
+        id: 18,
+        text: "使用密碼重置功能或尋求支持。",
+        isCorrect: true,
+        moneyLost: 0,
+      },
+    ],
+  },
+  {
+    id: 10,
+    text: "你收到了一個聲稱可以幫助你挖礦加密貨幣的網站，並且該網站聲稱可以免費為你挖礦。你會怎麼做？",
+    options: [
+      {
+        id: 19,
+        text: "提供個人和財務信息以進行挖礦。",
+        isCorrect: false,
+        moneyLost: 500000,
+      },
+      {
+        id: 20,
+        text: "進行研究並確認該網站的可信度和安全性。",
+        isCorrect: true,
+        moneyLost: 0,
+      },
+    ],
+  },
+];
