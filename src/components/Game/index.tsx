e185c5f0f88fa88a9f4a59ad7288b061e708e063
Nora Liu <noraliu0830@gmail.com>
2023-03-15 18:10:41 +0800

    fix: update main component for file extracting / add money bar

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,10 +1,19 @@
 import React, { useState } from "react";
-import styled from "styled-components";
+import {
+  GameContainer,
+  QuestionText,
+  OptionsList,
+  OptionButton,
+  NextButton,
+  MoneyBar,
+} from "./styles";
+import { questions } from "./questions";
 
 type Option = {
   id: number;
   text: string;
   isCorrect: boolean;
+  moneyLost: number;
 };
 
 type Question = {
@@ -13,85 +22,14 @@ type Question = {
   options: Option[];
 };
 
-const questions: Question[] = [
-  {
-    id: 1,
-    text: "What is the capital of France?",
-    options: [
-      { id: 1, text: "Paris", isCorrect: true },
-      { id: 2, text: "London", isCorrect: false },
-    ],
-  },
-  {
-    id: 2,
-    text: "What is the largest planet in our solar system?",
-    options: [
-      { id: 3, text: "Venus", isCorrect: false },
-      { id: 4, text: "Jupiter", isCorrect: true },
-    ],
-  },
-  // add more questions here
-];
-
-const GameContainer = styled.div`
-  max-width: 600px;
-  margin: 0 auto;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-`;
-
-const QuestionText = styled.p`
-  font-size: 24px;
-  font-weight: bold;
-  margin: 20px 0;
-`;
-
-const OptionsList = styled.ul`
-  list-style: none;
-  padding: 0;
-`;
-
-const OptionButton = styled.button<{ isSelected: boolean; isCorrect: boolean }>`
-  font-size: 18px;
-  padding: 10px;
-  margin: 10px;
-  border-radius: 8px;
-  border: none;
-  background-color: ${(props) =>
-    props.isSelected ? (props.isCorrect ? "#67C240" : "#E53935") : "white"};
-  color: ${(props) => (props.isSelected ? "white" : "black")};
-  cursor: pointer;
-  transition: background-color 0.3s ease;
-
-  &:hover {
-    background-color: ${(props) => (props.isCorrect ? "#2E7D32" : "#C62828")};
-  }
-`;
-
-const NextButton = styled.button`
-  font-size: 18px;
-  padding: 10px;
-  margin: 20px;
-  border-radius: 8px;
-  border: none;
-  background-color: #1e88e5;
-  color: white;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
-
-  &:hover {
-    background-color: #1565c0;
-  }
-`;
+const initialMoney = 1000000;
 
 const Game: React.FC = () => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(false);
   const [selectedOptionId, setSelectedOptionId] = useState<number | null>(null);
+  const [money, setMoney] = useState(initialMoney);
 
   const handleOptionClick = (optionId: number) => {
     setSelectedOptionId(optionId);
@@ -102,15 +40,19 @@ const Game: React.FC = () => {
       return;
     }
 
-    const currentQuestion = questions[currentQuestionIndex];
-    const selectedOption = currentQuestion.options.find(
+    const currentQuestion: Question = questions[currentQuestionIndex];
+    const selectedOption: Option | undefined = currentQuestion.options.find(
       (option) => option.id === selectedOptionId
     );
 
     if (selectedOption?.isCorrect) {
       setScore(score + 1);
     } else {
-      setGameOver(true);
+      const moneyLost = selectedOption?.moneyLost || 0;
+      setMoney((prevMoney) => prevMoney - moneyLost);
+      if (money - moneyLost <= 0) {
+        setGameOver(true);
+      }
     }
 
     setSelectedOptionId(null);
@@ -118,10 +60,11 @@ const Game: React.FC = () => {
     setGameOver(currentQuestionIndex === questions.length - 1);
   };
 
-  const currentQuestion = questions[currentQuestionIndex];
+  const currentQuestion: Question = questions[currentQuestionIndex];
 
   return (
     <GameContainer>
+      <MoneyBar money={money} />
       {gameOver ? (
         <>
           <QuestionText>
